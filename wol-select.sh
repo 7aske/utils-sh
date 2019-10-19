f81cdc786b7fcbd8434420a45a44464aafa1db97
Nikola Tasic <ntasic7@gmail.com>
2019-10-19 11:53:54 +0200

    wol-select script

---
+++ wol-select.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+mapfile -t options < hosts
+if [ "$?" -eq 1 ]; then
+    echo "Could not read hosts file"
+    exit 1
+fi
+for i in "${!options[@]}"
+do 
+    host="$(echo "${options[$i]}" | cut -d ' ' -f1)"
+    ip="$(echo "${options[$i]}" | cut -d ' ' -f2)"
+    mac="$(echo "${options[$i]}" | cut -d ' ' -f3)"
+    printf "%d) %-10s %10s %10s\n" "$((i+1))" "$host" "$ip" "$mac"
+done
+printf "%s" "Select the device: "
+read -r reply
+reply=$((reply-1))
+if [ "$reply" -lt $(("${#options[@]}"+1)) ]; then
+    host="$(echo "${options[$reply]}" | cut -d ' ' -f1)"
+    ip="$(echo "${options[$reply]}" | cut -d ' ' -f2)"
+    mac="$(echo "${options[$reply]}" | cut -d ' ' -f3)"
+    wakeonlan "$mac" &> /dev/null
+    printf "Waking up %s@%s\n" "$host" "$ip"
+else
+    echo "Invalid index"
+fi
+
