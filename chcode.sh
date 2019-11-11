289a4022db27a3e8a2c11ddb67f55546ac323fd5
Nikola Tasic <ntasic7@gmail.com>
2019-11-11 16:14:29 +0100

    chcode

---
+++ chcode.sh
@@ -0,0 +1,13 @@
+lvl1="$(dir -1 -d $CODE/* 2> /dev/null | sort | fzf --reverse --cycle)"
+declare lvl2
+if [ ! -z "$lvl1" ]; then
+    lvl2="$(dir -1 -d $lvl1/* 2> /dev/null | sort | fzf --reverse --cycle)"
+    if [ -z "$lvl2" ]; then
+        echo $lvl1
+    else
+        echo $lvl2
+    fi
+else
+    echo $CODE
+fi
+
