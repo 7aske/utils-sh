4d44c66693a4958ddf6bfcaff0b73dae0a85c40e
Nikola Tasic <ntasic7@gmail.com>
2020-01-15 02:38:33 +0100

    rcode

---
+++ rcode.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+repo=$1
+declare src
+declare dest
+
+if [ "$2" = "-s" ]; then
+	src="$3":"$CODE"/"$repo"
+	dest="$CODE"/"$repo"
+elif [ "$2" = "-d" ]; then
+	src="$CODE"/"$repo"
+	dest="$3":"$CODE"/"$repo"
+else
+	echo "Invalid second argument."
+	exit 1
+fi
+
+rsync --progress -have ssh "$src" "$(dirname "$dest")"
