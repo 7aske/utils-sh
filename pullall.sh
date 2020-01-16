45ac3dfe537aef7395c688d85e946e9d84778ec2
Nikola Tasic <ntasic7@gmail.com>
2020-01-16 03:18:24 +0100

    pullall fix when no notify-send

--- pullall.sh
+++ pullall.sh
@@ -8,12 +8,12 @@ if [ "$1" == "-A" ]; then
         d="$CODE/$lang"
         # echo $d
         for repo in $(dir "$d"); do
-            git -C "$d"/"$repo" pull 2> /dev/null | while read OUTPUT; do notify-send "$repo" "$OUTPUT"; done &
+            git -C "$d"/"$repo" pull 2> /dev/null | while read OUTPUT; do notify-send "$repo" "$OUTPUT" || echo "$repo" "$OUTPUT"; done &
         done
     done
 elif [ -z  "$1" ]; then
     for repo in "${repos[@]}"; do
         echo "$repo" && \
-        git -C "$CODE"/"$repo" pull 2> /dev/null | while read OUTPUT; do notify-send "$repo" "$OUTPUT"; done &
+        git -C "$CODE"/"$repo" pull 2> /dev/null | while read OUTPUT; do notify-send "$repo" "$OUTPUT" || echo "$repo" "$OUTPUT"; done &
     done
 fi
