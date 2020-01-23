fbbf8aabba7b8565cbe652cc702f6493d2ae86de
Nikola Tasic <ntasic7@gmail.com>
2020-01-23 16:26:18 +0000

    fixed addmode

--- addmode.sh
+++ addmode.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env sh
 
 output="Virtual-1"
+height="1080"
+width="1920"
+modename=""$width"x"$height""
+modeline="$(gtf 1920 1080 60 | grep 'Modeline ' | awk '{$1 = ""; $2 =""; print $0}')"
 
+echo "$modename$modeline"
 
-modeline="$(gtf 1920 1080 60 | grep 'Modeline ' | awk '{$1 = ""; print $0}')"
-echo "$modeline" | xargs xrandr --newmode
-xrandr --addmode "$output" "$(echo "$modeline" | awk '{print $1}')"
-xrandr --output "$output" --mode "$(echo "$modeline" | awk '{print $1}')"
+echo "$modename$modeline" | xargs xrandr --newmode && echo "Added $modename"
+
+xrandr --addmode "$output" "$modename" && echo "Added $modename to $output"
+
+xrandr --output "$output" --mode "$modename" && echo "Setting resolution of $output to $modename"
 
