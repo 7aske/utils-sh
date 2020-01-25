1f68e5fcd8ffd5929752e8fc4fcc3dab955bbb1f
Nikola Tasic <ntasic7@gmail.com>
2020-01-25 03:23:34 +0100

    better addmode with parameters

--- addmode.sh
+++ addmode.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env sh
 
-output="Virtual-1"
+output="$(xrandr | grep ' connected primary' | awk '{print $1}')"
+
+[ -z "$output" ] && echo "Unable to get primary output" && exit 1
+
 height="1080"
 width="1920"
-modename=""$width"x"$height""
-modeline="$(gtf 1920 1080 60 | grep 'Modeline ' | awk '{$1 = ""; $2 =""; print $0}')"
+
+if [ -n "$1" ] && [ -n "$2" ]; then
+    width="$1"
+    height="$2"
+fi
+
+modename="${width}x${height}"
+modeline="$(gtf "$width" "$height" 60 | grep 'Modeline ' | awk '{$1 = ""; $2 =""; print $0}')"
 
 echo "$modename$modeline"
 
@@ -13,4 +22,3 @@ echo "$modename$modeline" | xargs xrandr --newmode && echo "Added $modename"
 xrandr --addmode "$output" "$modename" && echo "Added $modename to $output"
 
 xrandr --output "$output" --mode "$modename" && echo "Setting resolution of $output to $modename"
-
