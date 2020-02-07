879cac8ea545ecdf73d6e405ba30b01a712d85f1
Nikola Tasic <ntasic7@gmail.com>
2020-02-07 22:54:07 +0100

    setwal sort

--- setwal.sh
+++ setwal.sh
@@ -2,12 +2,13 @@
 
 setwal_usage(){
     echo -e "  setval.sh <file>\n"
-    echo "      -l, list    lists all wallpapers in wallpaper dir"
+    echo "      -l, list      lists all wallpapers in wallpaper dir"
+    echo "      -h, --help    shows this message"
     exit 1
 }
 
 setwal_list(){
-    find "$WALS" -printf "%f\n"
+    find "$WALS" -printf "%f\n" | sort 
     exit 0
 }
 
@@ -16,6 +17,7 @@ WALS="$HOME"/Pictures/wallpaper
 WAL="$HOME"/.config/wallpaper
 
 [ "$1" = "-l" ] || [ "$1" = "list" ] && setwal_list
+[ "$1" = "-h" ] || [ "$1" = "--help" ] && setwal_usage
 [ -z "$1" ] && setwal_usage
 
 PIC="$(find "$WALS" -name "$1"\*)" 
